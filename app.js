0fcdd058f0bb735185d595a48fb7a5bc23fab8f0
Erick Rezende <erickbarrosrezende@gmail.com>
2025-04-20 14:19:24 -0300

    Enhances user authentication with cookies
    Refactors user authentication to use HTTP-only cookies for improved security.
    
    This change replaces the previous token-based authentication via headers with cookie-based authentication.
    The token is now stored in a secure, HTTP-only cookie, mitigating XSS risks.
    
    - Updates login endpoint to set the token as a cookie
    - Modifies authentication middleware to retrieve the token from the cookie
    - Configures CORS to allow credentials (cookies)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const { errorHandler } = require("./utils/errorHandler");
 const userRoutes = require("./routes/userRoutes");
@@ -11,7 +12,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:8080", // ou onde está seu front
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 // Development logging
 if (process.env.NODE_ENV === "development") {
