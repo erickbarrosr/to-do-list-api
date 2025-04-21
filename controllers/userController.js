5b8213ab183ac58b8a21f58bfa78da3852ad45d6
Erick Rezende <erickbarrosrezende@gmail.com>
2025-04-20 21:54:37 -0300

    refactor(userController.js): Alterar a mensagem de erro

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (userExists) {
     res.status(400);
-    throw new Error("User already exists");
+    throw new Error("Esse usuário já existe.");
   }
 
   // Create user
